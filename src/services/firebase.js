b986ef9f52e9dbd90f2084541188db75afdd44b8
Daniel Hsieh <dan2y006@gmail.com>
2020-06-21 18:17:09 +0800

    firebase init

---
+++ src/services/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyABVTP2r0oHXeYeiwMghMlpew1Q590tqu4",
+    authDomain: "real-time-database-edfa9.firebaseapp.com",
+    databaseURL: "https://real-time-database-edfa9.firebaseio.com",
+    projectId: "real-time-database-edfa9",
+    storageBucket: "real-time-database-edfa9.appspot.com",
+    messagingSenderId: "746790132554",
+    appId: "1:746790132554:web:c21c02bb9b20f3465e7fb3",
+    measurementId: "G-C1V4VKLV1D"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export const auth = firebase.auth;
+export const db = firebase.database()
