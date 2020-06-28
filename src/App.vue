d157fb232d09d16bcf7cfc3152784a93d53343a5
Daniel Hsieh <dan2y006@gmail.com>
2020-06-28 15:57:58 +0800

    update app.vue (route nav bar)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Login</router-link>|
       <router-link to="chat">Chat</router-link>
-    </div>
+    </div>-->
     <router-view />
   </div>
 </template>
